29dc71cf62c41ecd11d4ac9da047ab189b314cbf
catalinahawes <catalinahawes@gmail.com>
2026-06-08 13:29:15 -0300

    nueva funcion

---
+++ Funcion.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 13:11:07 2026
+
+@author: catalinahawes
+"""
+
+from ... 
+
+def mostrar_general(archivo):
+    '''
+    Lee un archivo CSV y muestra toda la tabla
+    
+    Parametros:
+        archivo: csv, con toda la informacion de la gente desaparecida
+        
+    Errores:
+        
+    Returns:
+        
+    '''
+    
+    
+    inscripciones_con_estudiantes.head()
+    
