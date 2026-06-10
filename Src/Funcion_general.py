240e8dc2ad606d8b75b3ad8697afd6797a03a2e6
catalinahawes <catalinahawes@gmail.com>
2026-06-10 11:07:18 -0300

    errores f.general

--- Src/Funcion_general.py
+++ Src/Funcion_general.py
@@ -6,21 +6,21 @@ Created on Mon Jun  8 13:11:07 2026
 @author: catalinahawes
 """
 
-from Src.Principal import ruta 
+from TP_Aplicado.Principal import ruta 
 
 def mostrar_general(archivo):
     '''
     Lee un archivo CSV y muestra toda la tabla
     
     Parametros:
-        archivo: csv, con toda la informacion de la gente desaparecida
+        archivo (DataFrame): datos cargados desde un CSV.
         
     Errores:
-        
-    Returns:
+        EmptyFileError: si el archivo se encuentra vacio
         
     '''
+    if archivo.empty:
+        raise EmptyFileError("[ERROR CRÍTICO] El archivo esta vacio")
     
-    archivo.head()
-    
+    print(archivo.head())
     
