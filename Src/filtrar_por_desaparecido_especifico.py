a5941eb5051fc38ce7e73a9c72f2aa2f674e5ba9
nunezmatilde <nunezm@udesa.edu.ar>
2026-06-10 15:31:44 -0300

    Create filtrar_por_desaparecido_especifico.py

---
+++ Src/filtrar_por_desaparecido_especifico.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+ARCHIVO = "Datos/informacion_usuarios_argentina_unico.csv"
+from funcion_validacion import validar_nombre_apellido
+
+
+def filtrar_participante(df, nombre_buscado):
+    """
+    Descripción: filtra el DataFrame según el nombre del participante.
+    Parámetros: 
+        df (DataFrame) tabla con los datos
+        nombre_buscado (str) nombre y apellido del participante
+    Retorno: DataFrame filas que coinciden o DataFrame vacío si no hay coincidencias
+    """
+    
+    if not nombre_buscado:
+        return pd.DataFrame()
+    
+    coincidencias = df[df['Nombre y Apellido'] == nombre_buscado]
+    
+    return coincidencias
+
+
+def mostrar_caso_especifico(df):
+    """
+    Descripción: solicita un nombre al usuario y muestra la información especifica de ese participante.
+    Parámetros: df (DataFrame)
+    Retorno: None
+    """
+    
+    nombre_buscado= input("Ingrese nombre y apellido: ").strip()
+    nombre_buscado= validar_nombre_apellido
+    resultado = filtrar_participante(df, nombre_buscado)
+    
+    if len(resultado) > 0:
+        print("Caso encontrado:")
+        print(resultado.to_string(index=False))
+    else:
+        print("No se encontraron registros")
+
+
+# acordarse de ponerlo de PROGRAMA PRINCIPAL
+df = pd.read_csv(ARCHIVO)
+mostrar_caso_especifico(df)
+
